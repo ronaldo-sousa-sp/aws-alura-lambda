e2ec07c9d295f8b79356cb790117bf13655b8261
Ronaldo N. Sousa <ronaldo.sousa@desenvolvesp.com.br>
2026-06-24 11:03:28 -0300

    feat: adicionando variáveis de ambiente

--- index.mjs
+++ index.mjs
@@ -1,11 +1,13 @@
 export const handler = async (event) => {
 
+    const example_var = process.env.EXAMPLE_VAR || 'default_value';
+
     const response = {
         statusCode: 200,
         body: JSON.stringify(event),
     };
 
-    console.log('Log example: ', event);
+    console.log('Log example: ', {event, example_var});
 
     return response;
 };
